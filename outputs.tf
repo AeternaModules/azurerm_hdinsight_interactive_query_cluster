f2f1243675cd9a9c11117ecdb5dd4b7543dfa04a
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:56:58 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "hdinsight_interactive_query_clusters" {
+  description = "All hdinsight_interactive_query_cluster resources"
+  value       = azurerm_hdinsight_interactive_query_cluster.hdinsight_interactive_query_clusters
+}
+
